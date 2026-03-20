0644b402cadf1531495728c1925779f5c80dbf8e
Silvia <szeamer@tolemi.com>
2026-03-20 09:16:15 -0400

    get secrets from process env

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -3,4 +3,9 @@ export const prerender = false;
 import { makeHandler } from '@keystatic/astro/api';
 import config from 'virtual:keystatic-config';
 
-export const ALL = makeHandler({ config });
+export const ALL = makeHandler({
+  config,
+  clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: process.env.KEYSTATIC_SECRET,
+});
